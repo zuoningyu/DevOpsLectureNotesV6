0e2e7c859788b8b9519ab9c7b72506cc3531d84a
YuJiangRen <yu.wang.jr@gmail.com>
2022-01-23 17:56:37 +1100

    wk3_bugfix

--- WK3_Dockerisation/dockerfile/4.console-dependency/run.sh
+++ WK3_Dockerisation/dockerfile/4.console-dependency/run.sh
@@ -8,7 +8,7 @@ docker build -t jr/console-citymatcher .
 
 echo
 echo "Running container ..."
-docker run -it --rm jr/console-citymatcher
+docker run --link citymatcher --rm jr/console-citymatcher
 
 echo "Cleaning up ..."
 docker kill $(docker ps -q)
